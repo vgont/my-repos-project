375bc5a30929d9d0bd4d5633cc02ca4486bad046
vgont <vgontijo34@gmail.com>
2023-09-09 16:49:19 -0300

    add ternary operator in Button component

--- src/components/SubmitButton/index.js
+++ src/components/SubmitButton/index.js
@@ -1,6 +1,7 @@
-export default function SubmitButton({ children, type }){
+export default function SubmitButton({ children, type, loading }){
+
     return(
-        <button type={type} className="border-0 bg-[#0d2636] rounded ml-2 px-4 flex justify-center items-center">
+        <button type={type} disabled={loading} className="border-0 bg-[#0d2636] rounded ml-2 px-4 flex justify-center items-center disabled:cursor-not-allowed disabled:opacity-50">
             {children}
         </button>
     )
